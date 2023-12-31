908460710aa86c9171af2c8752573e4624bb7d5b
Asim Jarrar <asim@kth.se>
2023-12-31 17:31:43 +0100

    en lösning

--- hang_man.py
+++ hang_man.py
@@ -1,8 +1,8 @@
 
 # start
 x=0
-z=0
 k=0
+min_lista = []
 
 password = input("write a word: ")
 length = len(password)
@@ -10,23 +10,28 @@ length = len(password)
 
 while x < length:
     x=(x+1)
+    min_lista.append("-")
 print (f"there are {x} letters in the word: {password}")
 
 
-min_lista = []
+
 while k < (length):
     
     letter = input ("guess a letter in the word: ")
     z=0
-   
+
+
     for a in password:
+        
         if letter[0] == password[z]:
+            del min_lista[z]
             min_lista.insert(z, password[z])
-            print(letter, z)
+            z=z+1
         else:
-            print("*")
-        z=z+1
+            z=z+1
+        
     k=k+1
+    
     print(min_lista)
 
   
